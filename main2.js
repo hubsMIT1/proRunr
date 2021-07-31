50d2295a11caff9a85f04c12ddcbf1161ad3edd0
hubsMIT1 <83615352+hubsMIT1@users.noreply.github.com>
2021-07-31 05:59:43 +0530

    Update main2.js

--- main2.js
+++ main2.js
@@ -269,7 +269,7 @@
 
                 }
                 draw() {
-                    c.font = '20px serif'
+                    c.font = '30px serif'
                     c.font = this.width + " " + this.height;
                     c.fillStyle = this.color;
                     c.fillText(this.text, this.x, this.y);
